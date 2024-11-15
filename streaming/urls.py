2406673f6da263be2f26e695bb02cf373e4e200b
iSEViper <jklas187@naver.com>
2024-11-15 15:55:58 +0900

    overall change by hyun

--- streaming/urls.py
+++ streaming/urls.py
@@ -6,6 +6,10 @@ app_name = 'streaming'
 
 urlpatterns = [
     path('upload/', views.upload_streaming_movie, name='upload_streaming_movie'),
-    path('api/movies/', views.StreamingMovieList.as_view(), name='movie-list'),
-    path('api/movies/<int:movie_id>/stream/', views.StreamVideo.as_view(), name='movie-stream'),
+    path('<int:movie_id>/', views.streaming_detail, name='streaming_detail'),
+    path('', views.streaming_movie_list, name='streaming_movie_list'),  # 영화 목록 페이지
+    path('movie/<str:movie_id>/', views.streaming_movie_detail, name='streaming_movie_detail'),  # 영화 상세 페이지
+    path('api/movies/', views.movie_list, name='movie_list'),
+    path('api/movies/<str:movie_id>/', views.movie_detail, name='movie_detail'),
+    path('poster/<str:poster_id>/', views.get_streaming_movie_poster_image, name='streaming_movie_poster'),
 ]
