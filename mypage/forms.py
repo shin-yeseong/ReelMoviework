ca8c362c105737a51c8219c5084d863c1f3ccd52
jaewon heo <heojaewon-andrew1112>
2024-11-18 16:35:27 +0900

    Resolve merge conflict in mypage/forms.py

--- mypage/forms.py
+++ mypage/forms.py
@@ -1,8 +1,4 @@
 from django import forms
-<<<<<<< HEAD
-# from common.models import User  # 커스텀 사용자 모델
-=======
->>>>>>> main
 
 class ProfileUpdateForm(forms.Form):
     first_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
