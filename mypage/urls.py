6b40f38f88ffe98997843661ac3ddbc395efa495
KoYounGun <2022112646@dgu.ac.kr>
2024-11-22 16:51:07 +0900

    mypage 구매, 후원, 만든 프로젝트 기능 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'mypage'
 
 urlpatterns = [
-    path('', views.mypage, name='mypage'),  # 마이페이지 메인
-    path('update-profile/', views.update_profile, name='update_profile'),  # 프로필 수정
-    path('change-password/', views.change_password, name='change_password'),  # 비밀번호 변경
+    path('', views.mypage, name='mypage'),
+    path('update-profile/', views.update_profile, name='update_profile'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('funded-movies/', views.funded_movies, name='funded_movies'),
+    path('my-projects/', views.my_projects, name='my_projects'),  # 내가 등록한 프로젝트
 ]
