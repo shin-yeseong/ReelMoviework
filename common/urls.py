71fd62d792663acd9e3db7f93204d1b252d0accd
jaewon heo <heojaewon-andrew1112>
2024-11-25 14:31:03 +0900

    허재원_funding/views.py 수정_1125

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'common'
+
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
